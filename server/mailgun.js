6130fb17cb1dd33fb901c7743c11b594bd68f830
Harry Xue <harry.xue@hotmail.com>
2020-04-09 16:53:31 -0500

    Revised mailgun send function

--- server/mailgun.js
+++ server/mailgun.js
@@ -1,16 +1,16 @@
 'use strict';
 
 const Mailgun = require('mailgun-js');
-const { mailAPIKey, mailDomain } = require('./settings.json');
+const { mailAPIKey , mailDomain, mailSender } = require('./settings.json');
 const mailgun = new Mailgun({ apiKey: mailAPIKey, domain: mailDomain });
 
 /**
  * Sends email with information specified by data
  * 
- * @param {Object} data
+ * @param {Promise} data
  */
 exports.send = function send (data) {
-    data.from = data.from || 'CSSA at UW-Madison <noreply@harryxue.com>';
+    data.from = data.from || mailSender;
     return mailgun.messages().send(data);
 }
 
