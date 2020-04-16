ceba9bb2c137a449cc2d3155529c3cee56dc2900
Harry Xue <harry.xue@hotmail.com>
2020-04-16 02:48:53 -0500

    Revised email templates

--- compile/methods.js
+++ compile/methods.js
@@ -4,20 +4,28 @@ const mustache = require('mustache');
 const { User } = require('../server/database');
 const fs = require('fs');
 const path = require('path');
-const template = fs.readFileSync(path.resolve('template.html'), 'utf-8');
 const mailgun = require('../server/mailgun');
+
+const templates = {
+    confirmAddr: fs.readFileSync(path.resolve('templates/confirm_address.html'), 'utf-8')
+}
 
 function findAddressWithNoRoomNum () {
     // Query criteria:
     // Person is near Madison, has wisc.edu account, but did not put in a room or apartment number
-    // This excludes anyone in Eagle Heights who put an apartment letter (APT X)
+    // This excludes anyone in Eagle Heights and Sheboygan
+    // Excludes those who put an apartment letter (APT X)
     // Query logic:
     /*
     (Person lives close to Madison)
         AND
+    (Person does not live in Sheboygan)
+        AND
+    (Person does not live in Eagle Heights)
+        AND
     (Person has a wisc.edu address)
         AND
-    (addr2 does not have the Eagle Heights apartment designator i.e. APT X)
+    (addr2 does not have letter apartment designator i.e. APT X)
         AND (
             (addr2 field is empty)
                 OR
@@ -29,8 +37,10 @@ function findAddressWithNoRoomNum () {
     const queryObj = {
         $and: [
             { area: { $ne: 8 } },
+            // { area: { $ne: 3 } },
+            // { area: { $ne: 2 } },
             { info: { $not: { $regex: '@' } } },
-            { addr2: { $not: { $regex: '((APT|APARTMENT|ROOM) [A-Z])|(^[A-Z]$)|(^[A-Z] .+$)' } } },
+            { addr2: { $not: { $regex: '((APT|APARTMENT|ROOM|RM|SUITE) [A-Z])|(^[A-Z]$)|(^[A-Z] .+$)' } } },
             { $or: [
                 { addr2: '' },
                 { addr2: { $not: { $regex: '\\d+' } } },
@@ -46,22 +56,55 @@ async function print () {
     const users = await findAddressWithNoRoomNum();
     for (let r of users) {
         const { name, email, address, addr1, addr2 } = r;
-        console.log(`${name} <${email}> \b\b\b\b\t\t\t 登记地址: ${address} \b\b\b\b\t\t\t decode1: "${addr1}", decode2: "${addr2}"`)
+        console.log(`${name} <${email}> \t\t 登记地址: ${address} \t\t decode1: "${addr1}", decode2: "${addr2}"`)
+    }
+    console.log(users.length);
+}
+
+function findHarry () {
+    return User.findOne({ wechat: 'harry1999' });
+}
+
+async function sendMail (i) {
+    const users = await findAddressWithNoRoomNum();
+
+    for (let user of users) {
+        const user = users[i];
+        const emailContent = mustache.render(templates.confirmAddr, {
+            name: user.name,
+            address: user.address,
+            addr1: user.addr1,
+            addr2: user.addr2 || '----'
+        });
+    
+        const response = await mailgun.send({
+            subject: '健康包信息登记：请确认房间号',
+            to: user.email,
+            html: emailContent
+        });
+
+        console.log(++i, user.email, response.message);
     }
 }
 
-function sendMail (i) {
-    const emailContent = mustache.render(template, {
-        name: 'XZY',
-        address: '888 Some St City State',
-        addr1: '888 SOME ST',
-        addr2: 'CITY ST'
-    });
+async function sendConfirmationToAll(i) {
+    // To be implemented
+}
 
-    return mailgun.send({
-        subject: '健康包登记表 - 地址信息更新',
-        html: emailContent
-    });
+/**
+ * 
+ * @param {String} plaintext 
+ */
+function encode (plaintext) {
+    const secret = SECRET;
+    const enc = [];
+    for (let i = 0; i < plaintext.length; i += 1) {
+        const keyC = secret[i % secret.length];
+        const encC = `${String.fromCharCode((plaintext[i].charCodeAt(0) + keyC.charCodeAt(0)) % 256)}`;
+        enc.push(encC);
+    }
+    const str = enc.join('');
+    return encodeURIComponent(Buffer.from(str, 'binary').toString('base64'));
 }
 
 module.exports = { findAddressWithNoRoomNum, print, sendMail };
